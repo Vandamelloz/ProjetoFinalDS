17a79b16fb2ebb28522cfa3880035240828f56cd
Vandamelloz <vandabarsa2004@gmail..com>
2025-11-14 21:15:17 -0300

    criação da estrutura com nodejs

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const Post = require('./models/post');
+const path = require('path');
+// Configuração do Handlebars
+app.engine('handlebars', handlebars.engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    }
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(bodyParser.json());
+app.set('view engine', 'handlebars');
+
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+//erro no bootstrap
+
+//ASSETS
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get("/cadastro", (req, res) => {
+   res.render('formulario');
+});
+
+app.use("", (req, res) => {
+    titulo= conteudo
+})
+const PORT = 8080;
+
+app.listen( PORT, () => {
+    console.log('Servidor iniciado na porta http://localhost:'+ PORT);
+})
+
